7adf76e7169c6f3403c12cc1a35f5eb46cca9c4f
piyawat <salapao45@gmail.com>
2016-11-12 12:04:39 +0700

    Intent to SignUp finish

--- app/src/main/java/kku/hattinard/piyawat/easykku/MainActivity.java
+++ app/src/main/java/kku/hattinard/piyawat/easykku/MainActivity.java
@@ -1,13 +1,35 @@
 package kku.hattinard.piyawat.easykku;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Eplicit จะประกาศตัวแปร
+    private Button signInButton, signUpButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind Widget
+        signInButton = (Button) findViewById(R.id.button2);
+        signUpButton= (Button) findViewById(R.id.button);
+
+        //Sign Up Controller
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SignUpActivity.class));
+
+            }
+        });
+
+
+    } //นี่คือเมททอด main Method
+} //Main class นี่คือ คลาสหลัก
